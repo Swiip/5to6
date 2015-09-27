bf9467247c1a373aef835121b3dba8b6af5daba9
Matthieu Lux <matthieu.lux@gmail.com>
2015-09-27 20:37:06 +0200

    update slides

---
+++ examples/const.js
@@ -0,0 +1,7 @@
+var foo = 1;
+const bar = 2;
+
+foo = 3;
+// foo === 3
+bar = 4;
+// Line 6: "bar" is read-only
---
+++ examples/let.js
@@ -0,0 +1,9 @@
+if (true) {
+  var foo = 1;
+  let bar = 2;
+}
+
+console.log(foo);
+// 1
+console.log(bar);
+// ReferenceError: bar is not defined
